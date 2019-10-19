f201648add4e5e242dbfb3d62c6d167661c18540
Raul Bravo <br4ca@hotmail.com>
2019-10-19 20:51:29 +0100

    Init

---
+++ TranslateExtension.cs
@@ -0,0 +1,46 @@
+﻿namespace AvaloniaBindingFallback.Localization
+{
+    using System;
+    using Avalonia.Data;
+    using Avalonia.Markup.Xaml;
+    using Avalonia.Markup.Xaml.MarkupExtensions;
+    using Portable.Xaml.Markup;
+
+    public class TranslateExtension : MarkupExtension
+    {
+        public TranslateExtension(string key)
+        {
+            this.Key = key;
+        }
+
+        public string Key { get; set; }
+
+        public string FallBack { get; set; }
+
+        public string Context { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var fallback = FallBack;
+            if (string.IsNullOrWhiteSpace(FallBack))
+            {
+                fallback = Key;
+            }
+
+            var keyToUse = Key;
+            if (!string.IsNullOrWhiteSpace(Context))
+            {
+                keyToUse = $"{Context}/{Key}";
+            }
+
+            var binding = new BindingExtension($"[{keyToUse}]")
+            {
+                Mode = BindingMode.OneWay,
+                Source = Translator.Instance,
+                FallbackValue = fallback,
+            };
+
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+}
---
+++ Translator.cs
@@ -0,0 +1,34 @@
+﻿namespace AvaloniaBindingFallback
+{
+    using System;
+    using System.ComponentModel;
+
+    public class Translator : INotifyPropertyChanged
+    {
+        private const string IndexerName = "Item";
+        private const string IndexerArrayName = "Item[]";
+
+        public string this[string key]
+        {
+            get
+            {
+                if (key.Equals("test", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return "Test translated";
+                }
+
+                return null;
+            }
+        }
+
+        public static Translator Instance { get; set; } = new Translator();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Invalidate()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(IndexerName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(IndexerArrayName));
+        }
+    }
+}
